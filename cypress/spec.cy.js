09bd13d251c27b548d6ca2c83b18bf1411430603
Thayse Dias <156327575+Thayse-Dias@users.noreply.github.com>
2025-09-04 08:50:34 -0300

    Update spec.cy.js
    Teste realizado utilizando o site para testes SERVEREST

--- cypress/spec.cy.js
+++ cypress/spec.cy.js
@@ -1,1 +1,8 @@
-
+describe('Teste de Login', () => {
+  it('Login com sucesso', () => {
+    cy.visit('https://front.serverest.dev/login')
+    cy.get('[data-testid="email"]').type('xxxxxxxxxxxxxxx')
+    cy.get('[data-testid="senha"]').type('xxxxxx')
+    cy.get('[data-testid="entrar"]').click() 
+  })
+})
